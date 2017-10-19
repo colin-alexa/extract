baa168905c476d95c7088e971ba47af588535021
Colin-Alexa Robinson <colin_robinson@condenast.com>
2017-10-19 18:58:21 -0400

    python reference implementation

---
+++ extract-ref.py
@@ -0,0 +1,12 @@
+from sys import stdin
+
+show = false
+
+for line in stdin:
+    if line.startsWith("not ok"):
+        show = True
+    if line.startsWith("ok"):
+        show = False
+
+    if show:
+        print line
